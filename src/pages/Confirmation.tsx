cdd631e8b6df66fe2e4d59162b94a555a4d3cd96
Ref-DevSa <ref.devsa@gmail.com>
2026-07-03 11:03:48 +0200

    Add booking confirmation page

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,8 +1,31 @@
+import { Link, useLocation } from "react-router-dom";
+
 function Confirmation() {
+  const location = useLocation();
+
+  const booking = location.state;
+
   return (
-    <div>
+    <main
+      style={{
+        textAlign: "center",
+        marginTop: "80px",
+      }}
+    >
       <h1>Booking Confirmed!</h1>
-    </div>
+
+      <p>Your booking has been submitted.</p>
+
+      <p>
+        <strong>Start Date:</strong> {booking.startDate}
+      </p>
+
+      <p>
+        <strong>End Date:</strong> {booking.endDate}
+      </p>
+
+      <Link to="/">Return Home</Link>
+    </main>
   );
 }
 
