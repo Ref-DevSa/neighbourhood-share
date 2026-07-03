d834be14206b513bd79d2ae5a75336b11fd5d7bd
Ref-DevSa <ref.devsa@gmail.com>
2026-07-03 11:11:33 +0200

    Add booking review route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ItemDetails from "./pages/ItemDetails";
 import Booking from "./pages/Booking";
 import Confirmation from "./pages/Confirmation";
+import BookingReview from "./pages/BookingReview";
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <Route path="/item/:id" element={<ItemDetails />} />
         <Route path="/booking/:id" element={<Booking />} />
         <Route path="/confirmation" element={<Confirmation />} />
+        <Route path="/booking-review" element={<BookingReview />} />
       </Routes>
     </BrowserRouter>
   );
